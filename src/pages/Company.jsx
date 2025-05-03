d51cb32714cd64b7e6e1eb5d47e46a0ea1169e79
mardomai <mardo.mai@ametikool.ee>
2025-05-04 00:04:32 +0300

    HHUAD

--- src/pages/Company.jsx
+++ src/pages/Company.jsx
@@ -7,7 +7,7 @@ function Company() {
   useEffect(() => {
     // Load Google Maps script
     const script = document.createElement('script');
-    script.src = `https://maps.googleapis.com/maps/api/js?key=AIzaSyD1CjXn507guHFrVxdycC347nwoYlxE3Cg`;
+    script.src = `https://maps.googleapis.com/maps/api/js?key=${import.meta.env.VITE_GOOGLE_MAPS_API_KEY}`;
     script.async = true;
     script.defer = true;
     script.onload = initMap;
